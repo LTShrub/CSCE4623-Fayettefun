e1ad2d7a9d2a43071d51b87847154b38e1d171d9
leogj99 <leoguzmanj99@gmail.com>
2023-11-26 13:49:45 -0600

    Create Event Actvity Added

---
+++ app/src/main/java/com/example/fayettefun/View/CreateEventView.kt
@@ -0,0 +1,12 @@
+package com.example.fayettefun.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fayettefun.R
+
+class CreateEventView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+    }
+}
