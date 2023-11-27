26b775884ac8cb9aaa949260b017f0c269f2b0bd
LTShrub <amadeom.costaldi@gmail.com>
2023-11-26 22:52:37 -0600

    Added LoginScreen code fixed google-services.

---
+++ app/src/main/java/com/example/fayettefun/View/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.example.fayettefun.View
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fayettefun.R
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
