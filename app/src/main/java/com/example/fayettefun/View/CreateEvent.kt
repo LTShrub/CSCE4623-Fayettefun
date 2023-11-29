ac8f8b3bbf24aaea8c89f1997b11ea98e37264ef
leogj99 <leoguzmanj99@gmail.com>
2023-11-29 12:59:10 -0600

    Create Event Class Update

--- app/src/main/java/com/example/fayettefun/View/CreateEvent.kt
+++ app/src/main/java/com/example/fayettefun/View/CreateEvent.kt
@@ -2,22 +2,27 @@ package com.example.fayettefun.View
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import com.example.fayettefun.R
 import java.util.Calendar
+import java.util.Locale
 
 class CreateEvent : AppCompatActivity() {
 
     // Instance variables
     private lateinit var nameEvent: EditText
-    private lateinit var address: EditText
+    private lateinit var addressEvent: EditText
     private lateinit var timeEvent: Button
     private lateinit var dateEvent: Button
     private lateinit var descriptionEvent: EditText
+    private lateinit var createButton: Button
+    private lateinit var geocoder: Geocoder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +30,12 @@ class CreateEvent : AppCompatActivity() {
 
         // Initialize components
         nameEvent = findViewById(R.id.event_name)
-        address = findViewById(R.id.event_address)
+        addressEvent = findViewById(R.id.event_address)
         timeEvent = findViewById(R.id.event_time)
         dateEvent = findViewById(R.id.event_date)
         descriptionEvent = findViewById(R.id.event_description)
+        createButton = findViewById(R.id.create_button)
+        geocoder = Geocoder(this, Locale.getDefault())
 
         // Button methods
         timeEvent.setOnClickListener { // Used to get the current time(hour and minute) calling the Calendar class
@@ -54,11 +61,34 @@ class CreateEvent : AppCompatActivity() {
             }, currentYear, currentMonth, currentDay).show()
         }
 
+        createButton.setOnClickListener { // When pressed an event will be create and added to the database and map
+            createEvent()
+        }
+
     }
 
     // This method will get the address entered by the user and will transform it to a geolocation 
-    private fun setAddress(){
+    private fun createEvent(){
+        val setName = nameEvent.text.toString()
+        val setTime = timeEvent.text.toString()
+        val setDate = dateEvent.text.toString()
+        val setDescription = descriptionEvent.text.toString()
+        var setLatitude = 0.0
+        var setLongitude = 0.0
 
+        // Gets the address entered by the user and will get latitude and longitude from it
+        val setAddress = geocoder.getFromLocationName(addressEvent.text.toString(), 1)
+        if(!setAddress.isNullOrEmpty()){
+            // Need to be transformed to string before creating MapPoint object
+            setLatitude = setAddress[0].latitude
+            setLongitude = setAddress[0].longitude
+        }
+        Log.d("Name", "$setName")
+        Log.d("Time", "$setTime")
+        Log.d("Date", "$setDate")
+        Log.d("Description", "$setDescription")
+        Log.d("Latitude", "$setLatitude")
+        Log.d("Longitude", "$setLongitude")
     }
 
 }
