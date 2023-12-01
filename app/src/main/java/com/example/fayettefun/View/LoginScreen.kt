e36f080f5249320d773bd4fc394f2013c5d12980
cphinric <parkerhinrichs914@gmail.com>
2023-12-01 11:10:36 -0600

    Basic screens

--- app/src/main/java/com/example/fayettefun/View/LoginScreen.kt
+++ app/src/main/java/com/example/fayettefun/View/LoginScreen.kt
@@ -1,12 +1,20 @@
 package com.example.fayettefun.View
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fayettefun.R
 
 class LoginScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Allow for edge-to-edge display
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
     }
 }
