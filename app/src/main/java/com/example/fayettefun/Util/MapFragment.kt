d9611acba6617529ed89e9f5c8bd23e3d498127d
LTShrub <amadeom.costaldi@gmail.com>
2023-12-01 19:23:18 -0600

    Added viewmodels and initialized FirebaseApp. Added ApplicationClass for app-wide initializations.

--- app/src/main/java/com/example/fayettefun/Util/MapFragment.kt
+++ app/src/main/java/com/example/fayettefun/Util/MapFragment.kt
@@ -138,4 +138,19 @@ class OpenStreetMapFragment : Fragment(), Marker.OnMarkerClickListener {
         mMap.invalidate()
     }
 
+    fun addActiveEventMarkers(activeEvents: List<MapPoint>) {
+        for (event in activeEvents) {
+            val marker = Marker(mMap)
+            marker.position = GeoPoint(event.latitude, event.longitude)
+            marker.snippet = event.description  // Use any property as a snippet
+            marker.icon = ResourcesCompat.getDrawable(resources, R.drawable.temp_even_icon, null)
+
+            // Add marker to the map
+            mMap.overlays.add(marker)
+        }
+
+        // Force map redraw
+        mMap.invalidate()
+    }
+
 }
