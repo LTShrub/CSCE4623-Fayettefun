0d2ea2b3803f994f26f93be83fbecb356bde524c
LTShrub <amadeom.costaldi@gmail.com>
2023-12-03 01:02:24 -0600

    Added Firebase/Google Authentication finished user dataclass.

--- app/src/main/java/com/example/fayettefun/View/LoginScreen.kt
+++ app/src/main/java/com/example/fayettefun/View/LoginScreen.kt
@@ -1,12 +1,98 @@
 package com.example.fayettefun.View
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fayettefun.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginScreen : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?){
+
+    private lateinit var buttonGoogle: Button
+    private lateinit var buttonEmail: Button
+    private lateinit var buttonSignUP: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private val signInLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data: Intent? = result.data
+            handleSignInResult(data)
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        buttonEmail = findViewById(R.id.buttonEmail)
+        buttonEmail.setOnClickListener{
+            tempOverride()
+        }
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        buttonGoogle = findViewById(R.id.buttonGoogle)
+        buttonGoogle.setOnClickListener {
+            signInWithGoogle()
+
+
+        }
+
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun handleSignInResult(data: Intent?) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        try {
+            // Google Sign-In was successful, authenticate with Firebase
+            val account = task.getResult(ApiException::class.java)!!
+            firebaseAuthWithGoogle(account.idToken!!)
+        } catch (e: ApiException) {
+            // Google Sign-In failed, update UI accordingly
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    // You can now save user details or navigate to the main activity
+                    // For example:
+                    val intent = Intent(this, Map::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign-in fails, display a message to the user.
+                    // You can handle the error here
+                }
+            }
+    }
+
+    private fun tempOverride(){
+        //TODO REMOVE ME ASAP
+        val intent = Intent(this, Map::class.java)
+        startActivity(intent)
+        finish()
     }
 }
