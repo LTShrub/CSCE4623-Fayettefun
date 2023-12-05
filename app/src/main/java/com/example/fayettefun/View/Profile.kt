1a9ce2a8b76dc884edd0725f9461ec145f5e7d11
leogj99 <leoguzmanj99@gmail.com>
2023-12-05 12:05:27 -0600

    Update

--- app/src/main/java/com/example/fayettefun/View/Profile.kt
+++ app/src/main/java/com/example/fayettefun/View/Profile.kt
@@ -15,8 +15,6 @@ class Profile : AppCompatActivity() {
     private lateinit var picture: ImageView
     private lateinit var name: TextView
     private lateinit var bio: TextView
-    private lateinit var mRegistrations: Button
-    private lateinit var createdEvents: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +24,6 @@ class Profile : AppCompatActivity() {
         picture = findViewById(R.id.profile_picture)
         name = findViewById(R.id.profile_name)
         bio = findViewById(R.id.profile_bio)
-        mRegistrations = findViewById(R.id.button1)
-        createdEvents = findViewById(R.id.button2)
 
-
-        mRegistrations.setOnClickListener {
-            Log.d("REG BUT", "Clicking")
-        }
-        createdEvents.setOnClickListener {
-            Log.d("CREA BUT", "Clicking")
-        }
     }
 }
